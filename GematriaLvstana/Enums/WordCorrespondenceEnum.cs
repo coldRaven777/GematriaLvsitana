0a785499b9a82460ec62d2922e99e29fe1c9f1c4
Miguel B <filipe90ice@gmail.com>
2025-05-04 16:56:24 +0100

    changed enum

--- GematriaLvstana/Enums/WordCorrespondenceEnum.cs
+++ GematriaLvstana/Enums/WordCorrespondenceEnum.cs
@@ -3,30 +3,30 @@
     public enum WordCorrespondenceEnum
  
     {
-        A = 1,     // Aleph (א)
-        B = 2,     // Bet (ב)
-        D = 4,     // Dalet (ד)
-        E = 5,     // He (ה)
-        F = 80,    // Pe (פ)
-        G = 3,     // gimel
-        H = 5,     // He (ה)
-        I = 10,     // Yod (י)
-        J = 10,    // Yod (י)
-        K = 100,    // Kaf (ק)
-        L = 30,    // Lamed (ל)
-        M = 40,    // Mem (מ)
-        N = 50,    // Nun (נ)
-        O = 70,    // ayin? 
-        P = 80,    // Pe (פ)
-        Q = 100,   // Qof (ק)
-        R = 200,   // Resh (ר)
-        S = 300,   // Shin (ש)
-        T = 400,   // Tav (ת)
-        U = 6,     // Vav (ו)
-        V = 6,     // Vav (ו)
-        W = 6,     // Vav (ו)
-        X = 60,    // Samekh (ס) or Samekh-Tzadi
-        Y = 10,    // Yod (י)
-        Z = 7      // Zayin (ז)
+        A = 1,     
+        B = 2,     
+        D = 4,     
+        E = 5,     
+        F = 80,    
+        G = 3,     
+        H = 5,     
+        I = 10,    
+        J = 10,    
+        K = 100,   
+        L = 30,    
+        M = 40,    
+        N = 50,    
+        O = 70,    
+        P = 80,    
+        Q = 100,   
+        R = 200,   
+        S = 300,   
+        T = 400,   
+        U = 6,     
+        V = 6,     
+        W = 6,     
+        X = 60,    
+        Y = 10,    
+        Z = 7      
     }
 }
